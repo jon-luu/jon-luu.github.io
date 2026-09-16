fdb1fa49067a3ac317ec9b4b66cfa055f4bc89f7
Jon Luu <jon.luu00@gmail.com>
2026-09-16 14:23:02 -0400

    fixed wireframe pages

--- csce242/excercises/04js/scripts.js/intro.js
+++ csce242/excercises/04js/scripts.js/intro.js
@@ -1,9 +1,28 @@
-document.getElementbyID("btn-show-message").onClick = () => {
-    document.getElementById("p-message").innerHTML = "Hello World!";
-    console.log(e);
+//Shows a message when the button is clicked
+document.getElementById("btn-show-message").onclick = (e) => {
+    document.getElementById("p-message").innerHTML = "Hello World";
+    e.target.innerHTML = "done!";
+};
+
+//styles the link when it's clicked
+document.getElementById("link").onclick = (e) => {
+    e.preventDefault(); //don't go to links destination
+    e.target.classList.add("cool-link");
+};
+
+//when button clicked make ball bounce
+document.getElementById("btn-bounce").onclick = (e) => {
+    document.getElementById("ball").classList.toggle("bouncing-ball");
 }
 
-//styles the link when clicked
-document.getElementById("link").onClick = (e) => {
-    e.target.className = "cool-link";
+//when you change the number od fays since you watered your plant
+//show a message and change the time
+document.getElementById("txt-num-days").onkeyup = (e) => {
+    const numDays = parsetInt(e.target.value);
+    const pMessage = document.getElementById("p-plant-msg");
+    pMessage.innerHTML = numDays;
+
+    if (numDays <= 2) {
+        pMessage.innerHTML = "Let your plant rest it's only been " + numDays + " days!";
+    }
 }
